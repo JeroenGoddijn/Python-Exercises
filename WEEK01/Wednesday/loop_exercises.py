22504d50e5161f0e707e65e3f07d7bd871e28ce2
Jeroen Goddijn <jeroen.goddijn@gmail.com>
2019-04-18 06:59:49 -0700

    Fixed exercises 7 and 8. Overcomplicated and tried printing pyramid vs triangle.

--- WEEK01/Wednesday/loop_exercises.py
+++ WEEK01/Wednesday/loop_exercises.py
@@ -46,16 +46,13 @@ for x in range(0, int(height)):
 # Print a triangle consisting of * characters like this:
 triangle_size = input("How wide do you want the triangle to be? ")
 for x in range(int(triangle_size)+1):
-    if x %2 != 0:
-        print("*"* int(x) )
-
-print("")
+    print("*"* int(x) )
 
 # 8. Print a Triangle II
 # Given a number as the height, print a triangle as in "Print a Triangle" but with the given height.
 triangle_height = input("How high do you want the triangle to be? ")
-for x in range(int(triangle_height)+1):
-    print("*")
+for x in range(1, int(triangle_height)+1):
+    print("*"* int(x))
 
 # 9. Multiplication Table
 # Print the multiplication table for numbers from 1 up to 10. 
@@ -66,10 +63,13 @@ for x in range(1,11):
 
 # Bonus: Print a Banner
 # Given a string, input by the user, print that string with a box around it. The box should stretch to the length of the string. Example session:
-#Text? Welcome to DigitalCrafts 
-#**************************** 
-#* Welcome to DigitalCrafts * 
-#****************************
+
+banner_string = input("What text would you like to display on the banner? ")
+for x in range(0, 3):
+    if x == 0 or x == 2:
+        print("*"* int(len(banner_string)+4))
+    else:
+        print("*", banner_string, "*")
 
 # Bonus: Triangle Numbers
 # Print the first 100 triangle numbers. What is a triangle number? Read this: https://www.mathsisfun.com/algebra/triangular-numbers.html.
