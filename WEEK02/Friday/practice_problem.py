43f2d2d657b3b47bb3c4ac5234a1d94b9bcb524c
Jeroen Goddijn <jeroen.goddijn@gmail.com>
2019-04-26 07:00:05 -0700

    Practice Problem - reverse each word in sentence

---
+++ WEEK02/Friday/practice_problem.py
@@ -0,0 +1,17 @@
+# Given a string, reverse each word in the sentence
+# i.e. "Hello World!"==> "olleH !dlrow"
+
+def reverse_words_in_sentence(sentence):
+    
+    # split the sentence
+    words = sentence.split()
+    reversed_words = []
+    
+    # for each word in the line:
+    for word in words:
+        # reverse each word and add it to the reversed_words list
+        reversed_words.append(word[::-1])
+
+    print(', '.join(reversed_words))
+
+reverse_words_in_sentence("Hello world!")
